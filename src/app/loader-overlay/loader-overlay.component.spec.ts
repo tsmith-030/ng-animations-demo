f2b8063d4be94015e2978f0ff72881b56b6a4b7d
Taylor Smith <pair+Taylor.Smith@CVSHealth.com>
2019-02-21 10:34:05 -0600

    Loader overlay component modified to toggle between fullscreen and containerized versions

--- src/app/loader-overlay/loader-overlay.component.spec.ts
+++ src/app/loader-overlay/loader-overlay.component.spec.ts
@@ -1,18 +1,19 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoaderOverlayComponent } from './loader-overlay.component';
+import {LoaderOverlayComponent} from './loader-overlay.component';
 import {MatProgressSpinnerModule} from '@angular/material';
+import {By} from '@angular/platform-browser';
 
-describe('Given LoaderOverlayComponent', () => {
+fdescribe('Given LoaderOverlayComponent', () => {
   let component: LoaderOverlayComponent;
   let fixture: ComponentFixture<LoaderOverlayComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoaderOverlayComponent ],
+      declarations: [LoaderOverlayComponent],
       imports: [MatProgressSpinnerModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -33,7 +34,7 @@ describe('Given LoaderOverlayComponent', () => {
   });
   describe('When a loading header IS provided but a loading message is NOT provided', () => {
     it('Then there is a spinner and a header but no message present', () => {
-      component.loadingHeader = "Header";
+      component.loadingHeader = 'Header';
       fixture.detectChanges();
       expect(fixture.nativeElement.querySelector('mat-spinner')).toBeTruthy();
       expect(fixture.nativeElement.querySelector('h2').innerText).toEqual('Header');
@@ -42,7 +43,7 @@ describe('Given LoaderOverlayComponent', () => {
   });
   describe('When a loading message IS provided but a loading header is NOT provided', () => {
     it('Then there is a spinner and a message but no message present', () => {
-      component.loadingMessage = "Idk, something";
+      component.loadingMessage = 'Idk, something';
       fixture.detectChanges();
       expect(fixture.nativeElement.querySelector('mat-spinner')).toBeTruthy();
       expect(fixture.nativeElement.querySelector('h2')).toBeFalsy();
@@ -51,12 +52,30 @@ describe('Given LoaderOverlayComponent', () => {
   });
   describe('When BOTH a loading message and loading header are provided', () => {
     it('Then there is a spinner and a message but no message present', () => {
-      component.loadingHeader = "Header";
-      component.loadingMessage = "Idk, something";
+      component.loadingHeader = 'Header';
+      component.loadingMessage = 'Idk, something';
       fixture.detectChanges();
       expect(fixture.nativeElement.querySelector('mat-spinner')).toBeTruthy();
       expect(fixture.nativeElement.querySelector('h2').innerText).toEqual('Header');
       expect(fixture.nativeElement.querySelector('p').innerText).toEqual('Idk, something');
+    });
+  });
+  describe('When fullscreen input is false', () => {
+    it('Then the spinner-overlay has the styling for a contained overlay', () => {
+      component.fullscreen = false;
+      fixture.detectChanges();
+      const spinnerOverlay = fixture.debugElement.query(By.css('#spinner-overlay')).nativeElement;
+
+      expect(spinnerOverlay.classList).toContain('contained-overlay');
+    });
+  });
+  describe('When fullscreen input is true', () => {
+    it('Then the spinner-overlay has the styling for a fullscreen overlay', () => {
+      component.fullscreen = true;
+      fixture.detectChanges();
+      const spinnerOverlay = fixture.debugElement.query(By.css('#spinner-overlay')).nativeElement;
+
+      expect(spinnerOverlay.classList).toContain('fullscreen-overlay');
     });
   });
 });
--- src/app/loader-overlay/loader-overlay.component.ts
+++ src/app/loader-overlay/loader-overlay.component.ts
@@ -8,4 +8,5 @@ import {Component, Input} from '@angular/core';
 export class LoaderOverlayComponent {
   @Input() loadingHeader;
   @Input() loadingMessage;
+  @Input() fullscreen;
 }
